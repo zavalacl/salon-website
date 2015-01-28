382e97f2f45277203f3308dfbd82865b039f9189
Claudia Zavala <zavalacl@msu.edu>
2015-01-28 13:31:11 -0500

    salon website

---
+++ js/gallery.js
@@ -0,0 +1,48 @@
+
+function OnPressImage1()
+{
+  var imageElement = document.getElementById("main picture");
+  imageElement.src = "img/homepic.jpg";
+}
+
+function OnPressImage2()
+{
+  var imageElement = document.getElementById("main picture");
+  imageElement.src = "img/hair1.jpg";
+}
+
+function OnPressImage3()
+{
+  var imageElement = document.getElementById("main picture");
+  imageElement.src = "img/hair2.jpg";
+}
+
+function OnPressImage4()
+{
+  var imageElement = document.getElementById("main picture");
+  imageElement.src = "img/hair3.jpg";
+}
+
+function OnPressImage5()
+{
+  var imageElement = document.getElementById("main picture");
+  imageElement.src = "img/makeup.jpg";
+}
+
+function OnPressImage6()
+{
+  var imageElement = document.getElementById("main picture");
+  imageElement.src = "img/yola.jpg";
+}
+
+function OnPressImage7()
+{
+  var imageElement = document.getElementById("main picture");
+  imageElement.src = "img/facial.jpg";
+}
+
+function OnPressImage8()
+{
+  var imageElement = document.getElementById("main picture");
+  imageElement.src = "img/kid.jpg";
+}
